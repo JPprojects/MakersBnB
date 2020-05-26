7e4f4e1da759ecad86efd9a53becf3f71d862470
Avin <avin.hurry@digital.education.gov.uk>
2020-05-26 15:47:32 +0100

    homepage/sign in tests passing

--- spec/feature/home_page_spec.rb
+++ spec/feature/home_page_spec.rb
@@ -1,18 +1,18 @@
 feature "should visit home page" do
-  scenario "home page should have content" do 
+  scenario "home page should have content" do
     visit '/'
     expect(page).to have_content "Welcome to Makers BnB"
   end
 
   feature "sign in" do
-    scenario "should visit home page and see sign in field" do 
+    scenario "should visit home page and see sign in field" do
       visit '/'
-      expect(page).to have_field "user_name"
+      expect(page).to have_field "username"
     end
 
     scenario "visit home page and see sign in button" do
       visit '/'
       expect(page).to have_button "Sign in"
     end
-  end 
+  end
 end
