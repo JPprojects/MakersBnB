e3b51f6c8b6bfd526e13f2da9f41c2b3170ded94
George Holborn <georgeholborn5@gmail.com>
2020-05-27 16:20:57 +0100

    Beginning of date to and date from

--- lib/bnb.rb
+++ lib/bnb.rb
@@ -1,28 +1,30 @@
 require 'pg'
 
 class Bnb
-  attr_reader :place, :id, :user_name, :description, :fixed_price, :reservation_status
+  attr_reader :place, :id, :user_name, :description, :fixed_price, :reservation_status, :date_to, :date_from
 
-  def initialize( id:, place:, user_name:, description:, fixed_price:, reservation_status:)
+  def initialize( id:, place:, user_name:, description:, fixed_price:, reservation_status:, date_from:, date_to:)
     @place = place
     @id = id
     @user_name = user_name
     @description = description
     @fixed_price = fixed_price
     @reservation_status = reservation_status
+    @date_to = date_to
+    @date_from = date_from
   end
 
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM main ;")
     result.map do |main|
-      Bnb.new( id: main['id'], place: main['place'], user_name: main['user_name'], description: main['description'], fixed_price: main['fixed_price'], reservation_status: main['reservation_status'] )
+      Bnb.new( id: main['id'], place: main['place'], user_name: main['user_name'], description: main['description'], fixed_price: main['fixed_price'], reservation_status: main['reservation_status'], date_from: main['date_from'], date_to: main['date_to'])
     end
   end
 
-  def self.create(user_name:, place:, description:, fixed_price:)
-    result = DatabaseConnection.query("INSERT INTO main (user_name, place, description, fixed_price ) VALUES('#{user_name}', '#{place}', '#{description}', '#{fixed_price}') RETURNING id, user_name, place, description, fixed_price, reservation_status")
-    Bnb.new(id: result[0]['id'], user_name: result[0]['user_name'], place: result[0]['place'],  description: result[0]['description'], fixed_price: result[0]['fixed_price'], reservation_status: result[0]['reservation_status'] )
+  def self.create(user_name:, place:, description:, fixed_price:, date_from:, date_to:)
+    result = DatabaseConnection.query("INSERT INTO main (user_name, place, description, fixed_price, date_from, date_to) VALUES('#{user_name}', '#{place}', '#{description}', '#{fixed_price}', '#{date_from}', '#{date_to}' RETURNING id, user_name, place, description, fixed_price, reservation_status, date_from, date_to")
+    Bnb.new(id: result[0]['id'], user_name: result[0]['user_name'], place: result[0]['place'],  description: result[0]['description'], fixed_price: result[0]['fixed_price'], reservation_status: result[0]['reservation_status'], date_from: result[0]['date_from'], date_to: result[0]['date_to'])
   end
 
 def self.find(id:)
--- spec/bnb_spec.rb
+++ spec/bnb_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Bnb do
       place = Bnb.create(place: "London", user_name:"george", description: "Good", fixed_price: "20")
       Bnb.create(place: "Berlin", user_name:"george", description: "Okay", fixed_price: "25")
       Bnb.create(place: "Casablanca", user_name:"george", description: "Good", fixed_price: "100")
-    
+
 
       places = Bnb.all
       p places
@@ -23,12 +23,14 @@ RSpec.describe Bnb do
 
   describe ".create" do
     it "Add a new place to stay§" do
-      place = Bnb.create(user_name: 'Bob', place: 'Casa Amor', description: 'The House of love, for wrinkly old people with too much money', fixed_price: '999')
+      place = Bnb.create(user_name: 'Bob', place: 'Casa Amor', description: 'The House of love, for wrinkly old people with too much money', fixed_price: '999', date_from: "2020-05-27", date_to: "2020-05-28")
 
       expect(place).to be_a Bnb
       expect(place.place).to eq 'Casa Amor'
       expect(place.description).to eq 'The House of love, for wrinkly old people with too much money'
       expect(place.fixed_price).to eq '999'
+      expect(place.date_from).to eq "2020-05-27"
+      expect(place.date_to).to eq "2020-05-28"
     end
   end
 end
