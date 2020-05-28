1a972094a2d1510c6559dcb96a451f250eff85ee
Anna <anna.voitik@gmail.com>
2020-05-28 11:23:58 +0100

    Random reservation id is generated and tests done

--- spec/feature/reserve_space_spec.rb
+++ spec/feature/reserve_space_spec.rb
@@ -22,5 +22,18 @@ feature 'review reservation' do
     expect(page).to have_content 'great ceilidh'
     expect(page).to have_content '100'
   end
+end
+
+feature 'reservation id' do
+  scenario 'user gets a reservation id' do
+    DatabaseConnection.query("INSERT INTO main (user_name, place, description, fixed_price) VALUES ('Anna', 'Glasgow', 'great ceilidh', '100');")
+    visit '/'
+    fill_in :username, with: 'Customer'
+    click_button 'Sign in'
+    click_button 'Reserve'
+    click_button 'Confirm'
+    expect(page).to have_content 'Your reservation id: '
+  end
 end 
+
 end
