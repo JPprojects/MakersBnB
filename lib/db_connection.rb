d7d2d8369eb7101d06adc8c93dc2dcfb0f3524fd
GlenHani <glen.hani@beis.gov.uk>
2020-05-29 11:27:45 +0100

    done

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -2,7 +2,8 @@ require './lib/database_connection'
 
 if ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup('Makers_BnB_test')
-elsif ENV['ENVIRONMENT'] == 'production'
+
+elsif ENV['RACK_ENV:'] == 'production'
   DatabaseConnection.setup('d78lc77l11dgnj')
 else 
   DatabaseConnection.setup('Makers_BnB')
